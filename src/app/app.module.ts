3c5a51c7e17efa68f090f3c3ec7d4cc7411eda3d
Gustavonvp <gustavonvp@gmail.com>
2021-12-17 14:07:17 -0300

    Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ElementsModule} from './elements/elements.module';
+import {CollectionsModule} from './collections/collections.module'
+
 
 @NgModule({
   declarations: [
     AppComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ElementsModule,
+    CollectionsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
