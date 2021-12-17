949af4daaf2bb89704dd29647e12c5636335a717
Gustavonvp <gustavonvp@gmail.com>
2021-12-17 15:33:49 -0300

    commit

--- src/app/collections/collections-home/collections-home.component.spec.ts
+++ src/app/collections/collections-home/collections-home.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CollectionsHomeComponent } from './collections-home.component';
 
@@ -6,12 +6,12 @@ describe('CollectionsHomeComponent', () => {
   let component: CollectionsHomeComponent;
   let fixture: ComponentFixture<CollectionsHomeComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
       declarations: [ CollectionsHomeComponent ]
     })
     .compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CollectionsHomeComponent);
--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -2,15 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CollectionsRoutingModule } from './collections-routing.module';
-import { CollectionsHomeComponent} from './collections-home/collections-home.component';
-
+import { CollectionsHomeComponent } from './collections-home/collections-home.component';
 
 @NgModule({
   declarations: [CollectionsHomeComponent],
-  imports: [
-    CommonModule,
-    CollectionsRoutingModule
-  ],
-  exports: [CollectionsHomeComponent]
+  imports: [CommonModule, CollectionsRoutingModule]
 })
-export class CollectionsModule { }
+export class CollectionsModule {}
