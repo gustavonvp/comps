f210f68d52d82b471372d47a4035e53b8967ae98
Gustavonvp <gustavonvp@gmail.com>
2021-12-17 20:36:13 -0300

    Using ng-content  property

--- src/app/shared/divider/divider.component.ts
+++ src/app/shared/divider/divider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-divider',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./divider.component.css']
 })
 export class DividerComponent implements OnInit {
+
+  @Input() title = ''
 
   constructor() { }
 
